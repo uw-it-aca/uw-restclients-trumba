684dab388151ef5d75b2f8343c6951ef5313bb07
Ethan Turner <ezturner@washington.edu>
2017-06-20 13:53:59 -0700

    Fixed URL in setup.py

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     license='Apache License, Version 2.0',
     description=('A library for connecting to the Trumba API'),
     long_description=README,
-    url="https://github.com/uw-it-aca/uw-restclients-iasystem",
+    url="https://github.com/uw-it-aca/uw-restclients-trumba",
     classifiers=[
         'Intended Audience :: Developers',
         'License :: OSI Approved :: Apache Software License',
